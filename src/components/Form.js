024d7a91ad342fe985e38678ca628794668a0bb5
Andre <andregpilakis@gmail.com>
2020-01-28 08:59:54 +0000

    added styling for page

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,7 @@ import React from "react";
 const Form = (props) => (
     <form onSubmit={props.getWeather}>
     <input type="text" name="city" placeholder="City"></input>
-    <input tawype="text" name="country" placeholder="Country"></input>
+    <input type="text" name="country" placeholder="Country"></input>
     <button >Get weather</button>
 </form>
     );
--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 
+//must use className instead of class when using JSX
 const Titles = () => (
     <div>
-    <h1>
+    <h1 className="title-container__title">
         Weather Finder
     </h1>
-    <p>Find out temperature conditions and more...</p>
+    <h3 className="title-container__subtitle">Find out temperature conditions and more...</h3>
 </div>
 );
 
